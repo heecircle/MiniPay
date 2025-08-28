f1f4fa0ccd75d92ef006f0c3dce8d53f6cc5b3b2
heecircle <yhw991228@gmail.com>
2025-08-28 16:16:21 +0900

    🎉 Initial: Begin Project & Set Environments

---
+++ src/test/java/com/kbds/minipay/MiniPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbds.minipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
